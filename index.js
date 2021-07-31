867b0bc8d44cbcbbc75dd5802714d343bedd09ae
Sophia Le <sophiale@sophias-mbp.lan>
2021-07-30 21:32:09 -0700

    fix typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
-const Person = require('./models/Person')
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(cors())
